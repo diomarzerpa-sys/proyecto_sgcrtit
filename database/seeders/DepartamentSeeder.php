829828cea63d853e527611d10906497d4cf4898c
diomarzerpa-sys <diomarzerpa@gmail.com>
2026-05-19 15:25:10 -0400

    depart

--- database/seeders/DepartamentSeeder.php
+++ database/seeders/DepartamentSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Department;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DepartamentSeeder extends Seeder
@@ -13,51 +12,55 @@ class DepartamentSeeder extends Seeder
      */
     public function run(): void
     {
-        Department::insert([
-            ['name' => 'Socio Académica'],
-            ['name' => 'PFG Salud Pública'],
-            ['name' => 'PFG Gestión Ambiental'],
-            ['name' => 'PNF Enfermería'],
-            ['name' => 'PFG Estudios Políticos Y Gobierno'],
-            ['name' => 'PFG Acuicultura Y Pesca'],
-            ['name' => 'PFG Comunicación Social'],
-            ['name' => 'PFG  Agro Ecología'],
-            ['name' => 'PFG Informática para la Gestión Social'],
-            ['name' => 'PFG Arquitectura'],
-            ['name' => 'Cultura'],
-            ['name' => 'PFG Gestión Social para el Desarrollo Local'],
-            ['name' => 'PFG Estudios Jurídicos'],
-            ['name' => 'Centro De Idiomas'],
-            ['name' => 'Apoyo Socio - Administrativo'],
-            ['name' => 'Talento Humano'],
-            ['name' => 'Consultoría Jurídica'],
-            ['name' => 'Producción y Recreación de Saberes'],
-            ['name' => 'Comunicación y Proyección Universitaria'],
-            ['name' => 'Dirección Del EGRKV'],
-            ['name' => 'Tecnología de Información y Telecomunicaciones'],
-            ['name' => 'CRISE'],
-            ['name' => 'Sala Situacional'],
-            ['name' => 'Salud Integral'],
-            ['name' => 'SIHO'],
-            ['name' => 'Seguridad Integral'],
-            ['name' => 'Deporte'],
-            ['name' => 'Desarrollo y Mantenimiento de Planta Física'],
-            ['name' => 'Servicios Generales'],
-            ['name' => 'Biblioteca'],
-            ['name' => 'Comedor'],
-            ['name' => 'Ingreso-Prosecución y Egreso Estudiantil'],
-            ['name' => 'Bienestar Estudiantil'],
-            ['name' => 'Transporte'],
-            ['name' => 'Subsecretaria'],
-            ['name' => 'Desempeño Estudiantil'],
-            ['name' => 'Coordinación De Sede'],
-            ['name' => 'PFG Ingenieria y Gestión Minera'],
-            ['name' => 'Postgrado'],
-            ['name' => 'PFG Enfermería Regional'],
-            ['name' => 'Centro de Estudios CEPEC'],
-            ['name' => 'Psicologia'],
-            ['name' => 'Laboratorio 11 de Informatica'],
-            ['name' => 'Laboratorio 12 de Informatica'],
-        ]);
+        $departments = [
+            'Socio Académica',
+            'PFG Salud Pública',
+            'PFG Gestión Ambiental',
+            'PNF Enfermería',
+            'PFG Estudios Políticos Y Gobierno',
+            'PFG Acuicultura Y Pesca',
+            'PFG Comunicación Social',
+            'PFG  Agro Ecología',
+            'PFG Informática para la Gestión Social',
+            'PFG Arquitectura',
+            'Cultura',
+            'PFG Gestión Social para el Desarrollo Local',
+            'PFG Estudios Jurídicos',
+            'Centro De Idiomas',
+            'Apoyo Socio - Administrativo',
+            'Talento Humano',
+            'Consultoría Jurídica',
+            'Producción y Recreación de Saberes',
+            'Comunicación y Proyección Universitaria',
+            'Dirección Del EGRKV',
+            'Tecnología de Información y Telecomunicaciones',
+            'CRISE',
+            'Sala Situacional',
+            'Salud Integral',
+            'SIHO',
+            'Seguridad Integral',
+            'Deporte',
+            'Desarrollo y Mantenimiento de Planta Física',
+            'Servicios Generales',
+            'Biblioteca',
+            'Comedor',
+            'Ingreso-Prosecución y Egreso Estudiantil',
+            'Bienestar Estudiantil',
+            'Transporte',
+            'Subsecretaria',
+            'Desempeño Estudiantil',
+            'Coordinación De Sede',
+            'PFG Ingenieria y Gestión Minera',
+            'Postgrado',
+            'PFG Enfermería Regional',
+            'Centro de Estudios CEPEC',
+            'Psicologia',
+            'Laboratorio 11 de Informatica',
+            'Laboratorio 12 de Informatica'
+        ];
+
+        foreach ($departments as $name) {
+            Department::firstOrCreate(['name' => $name]);
+        }
     }
 }
